fba73f0b462b1292c7ccaa40cfac7b027b4dab1c
lamquocminhhuy <lamquocminhhuy@gmail.com>
2021-09-20 23:29:14 +0700

    Connect MySQL & create , get products

---
+++ src/ui/app.js
@@ -0,0 +1,96 @@
+const { remote } = require('electron');
+const main = remote.require("./main.js");
+
+
+
+const productForm = document.getElementById('productForm');
+
+const productName = document.getElementById('name');
+const productPrice = document.getElementById('priceId');
+const productDescription = document.getElementById('description');
+const productSize = document.getElementById('size');
+const productAmount = document.getElementById('amount');
+const table = document.getElementById('table');
+
+// Count 
+const cardTitles = document.querySelectorAll('h3.card-title');
+
+// Render Product
+const productsList = document.getElementById('products')
+let products = []
+
+// Delete Product
+
+
+
+
+
+
+
+
+
+productForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+   
+    const newProduct = {
+        name: productName.value,
+        priceId: productPrice.value,
+        description: productDescription.value,
+        size: productSize.value,
+        amount: productAmount.value,
+    }
+    const result = await main.createProduct(newProduct);
+    console.log(result);
+  
+    getProducts();
+  
+})
+
+
+function renderProducts(products) {
+    var inStock = 0;
+    var outOfStock = 0;
+
+    products.forEach((product,index) => {
+        var row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${index+1}</td>
+            <td hidden id="productId">${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.description}</td>
+            <td>${product.priceId}</td>
+            <td>${product.amount}</td>
+            <td>${product.size}</td>
+            <td>${product.amount == 0 ? "Out of stock" : "In stock"}</td>
+            <td class="d-flex justify-content-center">
+            <a class="btn btn-sm btn-info" href="/update_order/4/">Update</a>
+            <button id="deleteProduct" type="submit" class="btn btn-danger">Delete</button>
+            </td>
+        `;
+        table.children[0].appendChild(row);
+        product.amount != 0 ? inStock++ : outOfStock++;
+    })
+
+    cardTitles[0].innerHTML = products.length; // Total product
+    cardTitles[1].innerHTML = inStock; // In stock
+    cardTitles[2].innerHTML = outOfStock; // Out of Stockv
+}
+
+const getProducts = async () => {
+    products = await main.getProducts();
+    renderProducts(products)
+    window.onload = ()=>{
+        setTimeout(()=>{
+            loading.style.display = 'none';
+        },1000);
+    }
+  
+ }
+
+ async function init () {
+      await getProducts();
+   
+      
+ }
+
+ init();
